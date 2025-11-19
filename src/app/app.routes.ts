91ce9716fac3e98ae2c24c874c71b305f28c71a5
Daniel Razafsky <drazafsky@shmarinas.com>
2025-11-18 23:35:33 -0600

    feat: integrate Tailwind CSS and update project structure for SCSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { IntroPage } from '../pages/intro-page/intro-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: IntroPage },
+  { path: 'quiz', loadChildren: () => import('../pages/quiz-list-page/quiz-list-page.routes').then(m => m.routes) },
+];
