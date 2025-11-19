0fc07dc92b099f5eef110cb0e02879a91454c73f
Daniel Razafsky <daniel@razafsky.com>
2025-11-19 00:45:24 -0600

    feat: restructure quiz module and add quiz details page components

---
+++ src/app/pages/quiz/quiz-details-page/quiz-details-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-details-page',
+  imports: [],
+  templateUrl: './quiz-details-page.html',
+  styleUrl: './quiz-details-page.scss',
+})
+export class QuizDetailsPage {
+
+}
