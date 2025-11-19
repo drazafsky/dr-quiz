123491a307173937bcca00a2aa3896f048513324
Daniel Razafsky <daniel@razafsky.com>
2025-11-18 23:35:33 -0600

    feat: integrate Tailwind CSS and update project structure for SCSS

---
+++ src/pages/quiz-list-page/quiz-list-page.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { QuizListPage } from './quiz-list-page';
+
+export const routes: Routes = [
+  { path: '', component: QuizListPage },
+];
---
+++ src/pages/quiz-list-page/quiz-list-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-list-page',
+  templateUrl: './quiz-list-page.html',
+  styles: [],
+})
+export class QuizListPage {}
