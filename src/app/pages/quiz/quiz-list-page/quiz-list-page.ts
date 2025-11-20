28c5dcf44c715421bd9dcb6c1d8a2bbe1e8e7da4
Daniel Razafsky <drazafsky@shmarinas.com>
2025-11-20 13:58:57 -0600

    feat: add Duration column to QuizListPage with formatted timeLimit
    feat: add DatePipe to quiz list page imports

--- src/app/pages/quiz/quiz-list-page/quiz-list-page.ts
+++ src/app/pages/quiz/quiz-list-page/quiz-list-page.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { QuizStore } from '../quiz.store';
 import { combineLatest, map } from 'rxjs';
 import { QuizListPageService } from './quiz-list-page-service';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
 import { Quiz } from '../types/quiz';
 import { ActivatedRoute, Router } from '@angular/router';
 import { toObservable } from '@angular/core/rxjs-interop';
@@ -10,7 +10,7 @@ import { toObservable } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-quiz-list-page',
   templateUrl: './quiz-list-page.html',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, DatePipe],
   styles: [],
   providers: [QuizStore, QuizListPageService],
 })
