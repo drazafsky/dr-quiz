999393d4cd71ec72096835a6f77a2d6d103f48a0
Daniel Razafsky <drazafsky@shmarinas.com>
2025-11-20 09:49:20 -0600

    feat: load quiz by quizId and display using async pipe in TakeQuiz component

--- src/app/pages/quiz/take-quiz/take-quiz-service.ts
+++ src/app/pages/quiz/take-quiz/take-quiz-service.ts
@@ -1,6 +1,25 @@
-import { Injectable } from '@angular/core';
+import { computed, inject, Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { QuizStore } from '../quiz.store';
 
 @Injectable({
+  providedIn: 'root',
+})
+export class TakeQuizService {
+  readonly #route = inject(ActivatedRoute);
+  readonly #quizStore = inject(QuizStore);
+
+  readonly quiz$ = computed(() => this.#quizStore.selectedQuiz());
+
+  constructor() {
+    this.#route.paramMap.subscribe(params => {
+      const quizId = params.get('quizId');
+      if (quizId) {
+        this.#quizStore.getById(quizId);
+      }
+    });
+  }
+}
   providedIn: 'root',
 })
 export class TakeQuizService {
