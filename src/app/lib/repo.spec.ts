82b98e2b9ca72b236575b08486c39bcb8661e692
Daniel Razafsky <drazafsky@shmarinas.com>
2025-11-20 00:46:28 -0600

    test: add zoneless change detection to quiz and repo tests

--- src/app/lib/repo.spec.ts
+++ src/app/lib/repo.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { Repo } from './repo';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('Repo', () => {
   let service: Repo;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideZonelessChangeDetection(),
+      ]
+    });
     service = TestBed.inject(Repo);
   });
 
--- src/app/pages/quiz/quiz-details-page/quiz-details-page-service.spec.ts
+++ src/app/pages/quiz/quiz-details-page/quiz-details-page-service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { QuizDetailsPageService } from './quiz-details-page-service';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { QuizStore } from '../quiz.store';
 
 describe('QuizDetailsPageService', () => {
   let service: QuizDetailsPageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideZonelessChangeDetection(),
+        provideRouter([]),
+        QuizStore,
+      ]
+    });
     service = TestBed.inject(QuizDetailsPageService);
   });
 
