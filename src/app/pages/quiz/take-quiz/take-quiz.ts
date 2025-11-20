36b4f65067798662421f3570c7f48e133a0c98c5
Daniel Razafsky <drazafsky@shmarinas.com>
2025-11-20 09:53:58 -0600

    feat: add unit tests for TakeQuizService and improve quiz data handling

--- src/app/pages/quiz/take-quiz/take-quiz.ts
+++ src/app/pages/quiz/take-quiz/take-quiz.ts
@@ -1,17 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { TakeQuizService } from './take-quiz-service';
+import { combineLatest, map } from 'rxjs';
+import { QuizStore } from '../quiz.store';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-take-quiz',
   imports: [AsyncPipe, JsonPipe],
-  providers: [TakeQuizService],
+  providers: [QuizStore, TakeQuizService],
   templateUrl: './take-quiz.html',
   styleUrl: './take-quiz.scss',
 })
 export class TakeQuiz {
-
   readonly #takeQuizService = inject(TakeQuizService);
 
-  vm$ = this.#takeQuizService.quiz$;
+  vm$ = combineLatest([
+    toObservable(this.#takeQuizService.quiz$),
+  ]).pipe(
+    map(([ quiz ]) => ({
+      quiz,
+    }))
+  );
 }
