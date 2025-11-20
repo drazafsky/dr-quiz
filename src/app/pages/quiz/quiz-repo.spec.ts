35122ec2fd3184a509bbcaf0983f41ee0a2e2f9e
Daniel Razafsky <drazafsky@shmarinas.com>
2025-11-20 00:42:00 -0600

    test: update QuizRepo tests to include zone change detection and mockRepo

--- src/app/pages/quiz/quiz-repo.spec.ts
+++ src/app/pages/quiz/quiz-repo.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { QuizRepo } from './quiz-repo';
+import { provideZoneChangeDetection } from '@angular/core';
+import { Repo } from '../../lib/repo';
 
 describe('QuizRepo', () => {
   let service: QuizRepo;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideZoneChangeDetection(),
+        {
+          provide: Repo, useValue: mockRepo
+        }
+      ]
+    });
     service = TestBed.inject(QuizRepo);
   });
 
