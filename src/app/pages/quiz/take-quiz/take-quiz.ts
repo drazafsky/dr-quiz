1d135cba581234057cfd21af88c66ca1ef2a1208
Daniel Razafsky <daniel@razafsky.com>
2025-11-20 09:49:20 -0600

    feat: load quiz by quizId and display using async pipe in TakeQuiz component

--- src/app/pages/quiz/take-quiz/take-quiz.ts
+++ src/app/pages/quiz/take-quiz/take-quiz.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { TakeQuizService } from './take-quiz-service';
 
 @Component({
   selector: 'app-take-quiz',
-  imports: [],
+  imports: [AsyncPipe, JsonPipe],
+  providers: [TakeQuizService],
   templateUrl: './take-quiz.html',
   styleUrl: './take-quiz.scss',
 })
 export class TakeQuiz {
 
+  readonly #takeQuizService = inject(TakeQuizService);
+
+  vm$ = this.#takeQuizService.quiz$;
 }
