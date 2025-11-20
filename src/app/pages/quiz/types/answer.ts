b717248ac64ca3d1a227f1ca508e604a624e7680
Daniel Razafsky <drazafsky@shmarinas.com>
2025-11-19 20:17:22 -0600

    feat: add quiz details form with questions and answers structure

--- src/app/pages/quiz/types/answer.ts
+++ src/app/pages/quiz/types/answer.ts
@@ -1,5 +1,4 @@
 export interface Answer {
-    id: number;
     value: string;
     isCorrect: boolean;
 }
--- src/app/pages/quiz/types/question.ts
+++ src/app/pages/quiz/types/question.ts
@@ -1,7 +1,6 @@
 import { Answer } from "./answer";
 
 export interface Question {
-    id: number;
     pointValue: number;
     prompt: string;
     answers: Answer[];
