3b6a280cbda770d7e0d87521ad38918d84fac553
Daniel Razafsky <drazafsky@shmarinas.com>
2025-11-20 10:10:18 -0600

    feat: update TakeQuiz component to use reactive forms for answers

--- src/app/pages/quiz/take-quiz/take-quiz.ts
+++ src/app/pages/quiz/take-quiz/take-quiz.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe, ReactiveFormsModule } from '@angular/common';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { TakeQuizService } from './take-quiz-service';
 import { combineLatest, map } from 'rxjs';
 import { QuizStore } from '../quiz.store';
@@ -7,13 +8,36 @@ import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-take-quiz',
-  imports: [AsyncPipe, JsonPipe],
+  imports: [AsyncPipe, JsonPipe, ReactiveFormsModule],
   providers: [QuizStore, TakeQuizService],
   templateUrl: './take-quiz.html',
   styleUrl: './take-quiz.scss',
 })
 export class TakeQuiz {
+  readonly #formBuilder = inject(FormBuilder);
   readonly #takeQuizService = inject(TakeQuizService);
+
+  readonly #form = this.#formBuilder.group({
+    questions: this.#formBuilder.array([]),
+  });
+
+  get questions(): FormArray {
+    return this.#form.get('questions') as FormArray;
+  }
+
+  constructor() {
+    this.vm$.subscribe(({ quiz }) => {
+      if (quiz) {
+        this.questions.clear();
+        quiz.questions.forEach((question) => {
+          const questionGroup = this.#formBuilder.group({
+            selectedAnswer: [''],
+          });
+          this.questions.push(questionGroup);
+        });
+      }
+    });
+  }
 
   vm$ = combineLatest([
     toObservable(this.#takeQuizService.quiz$),
