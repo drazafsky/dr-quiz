72216b15174be439c308add5bfedf54030138b8b
Daniel Razafsky <daniel@razafsky.com>
2025-11-20 09:39:27 -0600

    feat: add new route for TakeQuiz component in quiz routes

--- src/app/pages/quiz/quiz.routes.ts
+++ src/app/pages/quiz/quiz.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { QuizListPage } from './quiz-list-page/quiz-list-page';
 import { QuizDetailsPage } from './quiz-details-page/quiz-details-page';
+import { TakeQuiz } from './take-quiz/take-quiz';
 
 export const routes: Routes = [
   { path: '', component: QuizListPage },
   { path: ':quizId', component: QuizDetailsPage },
+  { path: ':quizId/take', component: TakeQuiz },
 ];
