7aaec7376ba3c2f0b6692e36744d967cfbbe2e0d
Daniel Razafsky <daniel@razafsky.com>
2025-11-21 15:11:21 -0600

    feat: add QuizDetailPage with route for quiz details by quizId

--- src/app/pages/quiz/quiz.routes.ts
+++ src/app/pages/quiz/quiz.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from "@angular/router";
 
 import { QuizListPage } from './list/quiz-list-page';
 
+import { QuizDetailPage } from './details/quiz-detail-page';
+
 export const routes: Routes = [
   { path: '', component: QuizListPage },
+  { path: ':quizId', component: QuizDetailPage },
 ];
