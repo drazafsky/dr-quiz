a39da3dab66877bca4bf44f9076788ca633b422f
Daniel Razafsky <daniel@razafsky.com>
2025-11-21 12:39:22 -0600

    refactor: remove unused QuizRepo and TestRepo files and tests

--- src/app/lib/repos/repo.ts
+++ src/app/lib/repos/repo.ts
@@ -7,13 +7,13 @@ export class Repo {
   setItem<T>(value: T): void {
     try {
       const jsonValue = JSON.stringify(value);
-      localStorage.setItem(this.STORAGE_KEY, jsonValue);
+      localStorage.setItem(this.#STORAGE_KEY, jsonValue);
     } catch (e) {}
   }
 
   getItem<T>(): T | null {
     try {
-      const jsonValue = localStorage.getItem(this.STORAGE_KEY);
+      const jsonValue = localStorage.getItem(this.#STORAGE_KEY);
       const value = jsonValue != null ? JSON.parse(jsonValue) : null;
       return value;
     } catch (e) {
@@ -23,7 +23,7 @@ export class Repo {
 
   removeItem(): void {
     try {
-      localStorage.removeItem(this.STORAGE_KEY);
+      localStorage.removeItem(this.#STORAGE_KEY);
     } catch (e) {}
   }
 }
