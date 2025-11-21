0dc8204f75baec9d7fcf513bebea0eeed2f12cf1
Daniel Razafsky <drazafsky@shmarinas.com>
2025-11-21 12:49:00 -0600

    feat: create QuestionRepo extending Repo with STORAGE_KEY 'QUESTIONS'

---
+++ src/app/lib/repos/question-repo.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@angular/core";
+import { Repo } from "./repo";
+import { Question } from "../types/question";
+
+@Injectable({ providedIn: 'root' })
+export class QuestionRepo extends Repo<Question> {
+  override readonly STORAGE_KEY: string = 'QUESTIONS';
+}
