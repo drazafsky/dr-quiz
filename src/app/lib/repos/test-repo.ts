17405911d8dcacdbf3b38c4707f7a6e115382c1c
Daniel Razafsky <drazafsky@shmarinas.com>
2025-11-21 13:03:12 -0600

    feat: create TestRepo class with unit tests for localStorage operations

---
+++ src/app/lib/repos/test-repo.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@angular/core";
+import { Repo } from "./repo";
+import { Test } from "../types/test";
+
+@Injectable({ providedIn: 'root' })
+export class TestRepo extends Repo<Test> {
+  override readonly STORAGE_KEY: string = 'TESTS';
+}
