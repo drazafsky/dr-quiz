13c6875204b8a385a8b869883e85d93e0a8601a8
Daniel Razafsky <daniel@razafsky.com>
2025-11-21 17:04:48 -0600

    feat: create QuestionStore for managing Question objects

---
+++ src/app/lib/stores/question.store.ts
@@ -0,0 +1,96 @@
+import { inject } from '@angular/core';
+import { QuestionRepo } from '../repos/question-repo';
+import { Question } from '../types/question';
+import { signalStore, withHooks, withMethods, withState } from '@ngrx/signals';
+import { patchState } from '@ngrx/signals-store';
+import { setLoading, stopLoading } from './loading-feature';
+
+type QuestionState = {
+  questions: Question[];
+  selectedQuestionId: string | undefined;
+  loading: boolean;
+  save: {
+    success: boolean | undefined;
+    error: boolean | undefined;
+  };
+};
+
+const initialState: QuestionState = {
+  questions: [],
+  selectedQuestionId: undefined,
+  loading: false,
+  save: {
+    success: undefined,
+    error: undefined,
+  },
+};
+
+export const QuestionStore = signalStore(
+  withState(initialState),
+  withMethods((state, questionRepo = inject(QuestionRepo)) => {
+    return {
+      selectQuestion(selectedQuestionId: string | undefined) {
+        patchState(state, { selectedQuestionId });
+      },
+
+      saveQuestion(newQuestion: Question) {
+        patchState(state, setLoading());
+        patchState(state, { save: { success: undefined, error: undefined } });
+
+        let selectedQuestionId = state.selectedQuestionId();
+
+        if (
+          selectedQuestionId === 'create' ||
+          !selectedQuestionId ||
+          selectedQuestionId === ''
+        ) {
+          selectedQuestionId = crypto.randomUUID();
+          newQuestion.id = selectedQuestionId;
+          patchState(state, {
+            questions: [...state.questions(), newQuestion],
+            selectedQuestionId,
+          });
+        } else {
+          const updatedQuestions = state.questions().map((question) =>
+            question.id === selectedQuestionId ? { ...newQuestion, id: question.id } : question
+          );
+          patchState(state, { questions: updatedQuestions });
+        }
+
+        try {
+          questionRepo.setItem(state.questions());
+          patchState(state, { save: { success: true, error: undefined } });
+        } catch (e) {
+          patchState(state, { save: { success: undefined, error: true } });
+        } finally {
+          patchState(state, stopLoading());
+        }
+      },
+    };
+  }),
+  withHooks((state) => {
+    const questionRepo = inject(QuestionRepo);
+    return {
+      onInit() {
+        patchState(state, setLoading());
+        const questions = questionRepo.getItem();
+        if (questions !== null) {
+          patchState(state, { questions });
+        } else {
+          patchState(state, { questions: [] });
+        }
+        patchState(state, stopLoading());
+      },
+      onDestroy() {
+        patchState(state, setLoading());
+        const questions = questionRepo.getItem();
+        if (questions !== null) {
+          questionRepo.setItem(questions);
+        } else {
+          questionRepo.removeItem();
+        }
+        patchState(state, stopLoading());
+      },
+    };
+  })
+);
