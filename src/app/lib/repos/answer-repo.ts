dd5e7b8a85779e641623a6c66ee2146d0569ea23
Daniel Razafsky <daniel@razafsky.com>
2025-11-21 13:07:01 -0600

    feat: create AnswerRepo class with unit tests for localStorage operations

---
+++ src/app/lib/repos/answer-repo.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@angular/core";
+import { Repo } from "./repo";
+import { Answer } from "../types/answer";
+
+@Injectable({ providedIn: 'root' })
+export class AnswerRepo extends Repo<Answer> {
+  override readonly STORAGE_KEY: string = 'ANSWERS';
+}
