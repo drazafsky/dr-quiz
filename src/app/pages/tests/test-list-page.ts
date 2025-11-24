f15597f8e5d318c805e57dcc8739f4ab6d1f251b
Daniel Razafsky <daniel@razafsky.com>
2025-11-24 09:05:49 -0600

    feat: display quiz information in test list page from QuizStore
    feat: add TestDetailPage component for editing test details
    
    refactor: reorganize imports and update test list button logic

--- src/app/pages/tests/test-list-page.ts
+++ src/app/pages/tests/test-list-page.ts
@@ -4,21 +4,38 @@ import { CardComponent } from '../../lib/components/card/card';
 import { ToolbarComponent } from '../../lib/components/toolbar/toolbar.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TestStore } from '../../lib/stores/test.store';
+import { QuizStore } from '../../lib/stores/quiz.store';
+import { computed } from '@angular/core';
 import { Test } from '../../lib/types/test';
+import { Quiz } from '../../lib/types/quiz';
+
+interface TestWithRelatedQuiz extends Test {
+  quiz: Quiz;
+}
 
 @Component({
   selector: 'app-test-list-page',
   standalone: true,
   imports: [CommonModule, CardComponent, ToolbarComponent],
   templateUrl: './test-list-page.html',
-  providers: [TestStore],
+  providers: [QuizStore, TestStore],
 })
 export class TestListPage {
   readonly #testStore = inject(TestStore);
+  readonly #quizStore = inject(QuizStore);
   readonly #router = inject(Router);
   readonly #route = inject(ActivatedRoute);
 
-  tests$ = this.#testStore.tests;
+  quizzes$ = this.#quizStore.quizzes;
+  tests$ = computed(() => {
+    const tests = this.#testStore.tests();
+    const quizzes = this.quizzes$();
+
+    return tests.map(test => ({
+      ...test,
+      quiz: quizzes.find(quiz => quiz.id === test.quizId),
+    }));
+  });
 
   handleNew() {
     this.#router.navigate(['create'], { relativeTo: this.#route });
