2416c1ec8f3b667dcfb68872d577eb079647cd8e
Daniel Razafsky <drazafsky@shmarinas.com>
2025-11-24 00:32:42 -0600

    feat: add route for AnswerDetailPage in quiz.routes.ts

--- src/app/pages/quiz/quiz.routes.ts
+++ src/app/pages/quiz/quiz.routes.ts
@@ -5,9 +5,11 @@ import { QuizListPage } from './list/quiz-list-page';
 import { QuizDetailPage } from './details/quiz-detail-page';
 
 import { QuestionDetailPage } from './questions/detail/question-detail-page';
+import { AnswerDetailPage } from './answers/detail/answer-detail-page';
 
 export const routes: Routes = [
   { path: '', component: QuizListPage },
   { path: ':quizId', component: QuizDetailPage },
   { path: ':quizId/question/:questionId', component: QuestionDetailPage },
+  { path: ':quizId/question/:questionId/answer/:answerId', component: AnswerDetailPage },
 ];
