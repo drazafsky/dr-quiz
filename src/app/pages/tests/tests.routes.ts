df19b11aeeea1c9e938f656d130d4ef453b0577b
Daniel Razafsky <drazafsky@shmarinas.com>
2025-11-24 11:48:03 -0600

    feat: add route for TestDetailPage with testId parameter handling

--- src/app/pages/tests/tests.routes.ts
+++ src/app/pages/tests/tests.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { TestListPage } from './test-list-page';
 
+import { TestDetailPage } from './details/test-detail-page';
+
 export const routes: Routes = [
   { path: '', component: TestListPage },
+  { path: ':testId', component: TestDetailPage },
 ];
