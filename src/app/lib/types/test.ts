108176db204c53a66b275cbc823aceb9196741eb
Daniel Razafsky <daniel@razafsky.com>
2025-11-24 08:24:33 -0600

    feat: add TestListPage and lazy load tests routes in app.routes.ts
    feat: create TestStore to manage Test instances

--- src/app/lib/types/test.ts
+++ src/app/lib/types/test.ts
@@ -3,7 +3,7 @@ export interface Test {
     quizId: string;
     questions: string[]; // List of the questions to display to the user in the order to display them
     isSubmitted: boolean;
-    timeTaken: number; // Number of seconds the user has been working on the test
+    deadLine: Date; // When the test must be finished
     score: {
         correct: number;
         incorrect: number;
---
+++ src/app/pages/tests/tests.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { TestListPage } from './test-list-page';
+
+export const routes: Routes = [
+  { path: '', component: TestListPage },
+];
